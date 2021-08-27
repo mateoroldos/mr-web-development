32b1d35e603ff0aa6f97da8402d622cf3e6ad2bf
mateoroldos <mateoroldos19@gmail.com>
2021-08-27 17:16:24 -0300

    feat: :lipstick: add colors to theme.js

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,8 +1,24 @@
 import { extendTheme } from "@chakra-ui/react"
 const theme = {
   colors: {
-    mrgreen: "#75DDA9",
-    mrblack: "#16181C",
+    default: {
+      light: "#FFFFFF",
+      dark: "#16181C",
+    },
+    primary: {
+      100: "#7CEBB3",
+      200: "#76DEAA",
+      300: "#68C496",
+      400: "#549E79",
+      500: "#325E48",
+    },
+    secondary: {
+      100: "#EBA171",
+      200: "#DE996A",
+      300: "#C4875E",
+      400: "#9E6D4C",
+      500: "#5E412D",
+    },
   },
   fonts: {
     heading: "'Rubik', sans-serif",
