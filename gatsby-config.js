f89642821ad835ee19635ea4f0ff714341da602c
mateoroldos <mateoroldos19@gmail.com>
2021-08-27 18:00:36 -0300

    chore: :recycle: add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MR Web Development`,
+    description: `21st century web development.`,
+    author: `info@mateoroldos.com`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
